1c94ea908f7a18cf9a93a11d4fd930a874661c33
G-VelaLeon <vgladysleon@gmail.com>
2020-12-22 19:57:19 -0600

    Inicio Proyecto de Examen Git 2020.

---
+++ Gladys-Itics-2020/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Gladys_Itics_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parte Inicial del Proyecto");
+            Console.WriteLine("Registro de Series Televisivas");
+            //Crear Diccionario
+            //Variables
+            //Menú de Selección
+            Console.WriteLine("Fin del Proyecto");
+        }
+    }
+}
